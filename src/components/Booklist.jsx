fa3031849602ffcde5abf993c85cbf5ab548ec38
Tomo1-ifbb <tomo.soul.15@gmail.com>
2020-05-29 08:31:34 +0900

    第一回講義前までの実装

--- src/components/Booklist.jsx
+++ src/components/Booklist.jsx
@@ -1,12 +1,30 @@
 //App.jsx from parent component
 //This component means child component
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Booklist = (props) => {
-  const result = props.getData?.(props.language); //?→getDataが存在する時のみで関数を実行できる
+  const [bookData, setBookData] = useState(null);
+  useEffect(() => {
+    const result = props
+      .getData?.(props.language)
+      .then((response) => setBookData(response));
+  }, [props]);
+
   return (
     <div>
-      <p>This is {result} list component</p>
+      <ul>
+        {bookData === null ? (
+          <p>now loading...</p>
+        ) : (
+          //bookData.data.items.map((x, index) => <li>{x.volumeInfo.title}</li>)
+          bookData.data.items.map((x, index) => (
+            <li key={index}>
+              {x.volumeInfo.readingModes.image}
+              {x.volumeInfo.title}　名前：{x.volumeInfo.authors}
+            </li>
+          ))
+        )}
+      </ul>
     </div>
   );
 };
